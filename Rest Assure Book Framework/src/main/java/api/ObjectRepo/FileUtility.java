b00de4819cee2b8d3ed32fa5b7125cf5e227830b
Mukesh <cmukesh391@gmail.com>
2023-12-30 09:03:47 +0530

    Third commit

--- Rest Assure Book Framework/src/main/java/api/ObjectRepo/FileUtility.java
+++ Rest Assure Book Framework/src/main/java/api/ObjectRepo/FileUtility.java
@@ -9,7 +9,7 @@ public class FileUtility
    
 	public String ReadDatafromPropfile(String key) throws IOException 
 	{
-		FileInputStream fis = new FileInputStream("C:\\Users\\Mukesh\\Desktop\\API Framework\\untitled\\src\\test\\java\\utilities\\Endpoints.properties");
+		FileInputStream fis = new FileInputStream("C:\\Users\\Mukesh\\git\\repository\\Rest Assure Book Framework\\src\\test\\java\\utilities\\Endpoints.properties");
 		Properties prop = new Properties();
 		prop.load(fis);
 		return prop.getProperty(key,"Incorrectkey");	
